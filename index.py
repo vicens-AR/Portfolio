c19c7f17cc2d21f9515fd260f4a0ff9ff10c3eeb
Vicente Arhex Harwigt <d47797531@instituto.itr.edu.ar>
2024-11-21 10:55:43 -0300

    anda el cambio de img(sin el login)

--- index.py
+++ index.py
@@ -2,10 +2,14 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, login_user, login_required, logout_user, UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
+import os
+from time import time
 
 app = Flask(__name__)
 app.secret_key = 'SMHvYzo8dnjWs49Gzvjr'  # Cambia esta clave por algo seguro
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://uypspzas3lbrauuj:SMHvYzo8dnjWs49Gzvjr@brfj39ld4kifwyrcpnzj-mysql.services.clever-cloud.com:3306/brfj39ld4kifwyrcpnzj'
+UPLOAD_FOLDER = 'static'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 
 db = SQLAlchemy(app)
 login_manager = LoginManager(app)
@@ -22,7 +26,27 @@ class Skill:
     def __init__(self, name, image_url):
         self.name = name
         self.image_url = image_url
-    
+
+
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+#modo edicion(colo para la foto)
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/update-profile-image', methods=['POST'])
+def update_profile_image():
+    if 'profile_image' not in request.files:
+        return "No file part", 400
+    file = request.files['profile_image']
+    if file.filename == '':
+        return "No selected file", 400
+    if file and allowed_file(file.filename):
+        filename = 'profile_image.jpg'  # O usa `secure_filename` para un nombre único
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return redirect(url_for('principal'))
+    return "Invalid file type", 400
+
 # Cargar usuario
 @login_manager.user_loader
 def load_user(user_id):
@@ -96,7 +120,7 @@ def principal():
         Skill(name="GitHub", image_url="https://cdn-icons-png.flaticon.com/128/3291/3291695.png"),
         Skill(name="C++", image_url="https://cdn-icons-png.flaticon.com/128/6132/6132222.png"),
     ]
-    return render_template('Inicio.html', skills=skills_list)
+    return render_template('Inicio.html', skills=skills_list, time=time())
 
 if __name__ == '__main__':
     with app.app_context():
